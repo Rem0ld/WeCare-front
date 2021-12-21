c367309e6e0da83e7e6d3a2e3ed5dfa58cfa6414
Rem0ld <60188728+Rem0ld@users.noreply.github.com>
2021-12-21 16:02:04 +0100

    added bootstrap + router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Counter from "./features/counter/Counter";
+import Login from "./pages/Login/Login";
 
 function App(): JSX.Element {
   return (
     <div className="App">
       <Counter />
+      <Login />
     </div>
   );
 }
